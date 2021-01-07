017d73dcbc29a187afd74cc55e9fa272fd0b1d98
Michael Zhao <michaelzhao314@gmail.com>
2021-01-07 11:23:32 -0600

    PRETIER

---
+++ util.js
@@ -0,0 +1,171 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import { EventInfo } from './entries';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+function getId() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('id');
+}
+
+/**
+ * Parses a time using dayjs and returns the seconds in milliseconds.
+ * See the docs for dayjs: https://day.js.org/docs/en/timezone/timezone
+ *
+ * @param {string} input String time input for dayjs to parse
+ * @param {string} tz Tz database time zone (https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+ * @returns {number} Milliseconds since Jan 1, 1970 [UTC time]
+ */
+function parseTimeZone(input, tz) {
+    return dayjs.tz(input, tz).valueOf();
+}
+
+/**
+ * Offsets a millisecond UTC to a dayjs object in the correct timezone
+ * See the docs for dayjs: https://day.js.org/docs/en/timezone/timezone
+ *
+ * @param {string} millis UTC millisecond time
+ * @param {string} [tz] Tz database time zone; If left blank, the timezone will be guessed
+ * @returns {dayjs.Dayjs} Dayjs object with the correct timezone
+ */
+function convertToTimeZone(millis, tz) {
+    if (tz == undefined) {
+        var tz = dayjs.tz.guess();
+    }
+    return dayjs(Number(millis)).tz(tz);
+}
+
+/**
+ * Takes a list of sorted events and injects date seperators
+ *
+ * @param {EventInfo[]} eventList List of events, with an extra dayjs object defined
+ */
+function divideByDate(eventList) {
+    var currDay = '';
+    for (var i = 0; i < eventList.length; i++) {
+        var day = eventList[i].startDayjs.format('YYYY-MM-DD');
+        if (currDay != day) {
+            currDay = day;
+            eventList.splice(i, 0, {
+                isDate: true,
+                day,
+            });
+            i++;
+        }
+    }
+}
+
+/**
+ * Converts a date to a readable date section label
+ *
+ * @param {string} date Date passed in formatted as YYYY-MM-DD
+ * @return {string} Readable date section label
+ */
+function createDateHeader(date) {
+    return dayjs(date).format('dddd M/D/YY');
+}
+
+/**
+ * Gets the starting and ending time display for an event
+ *
+ * @param {EventInfo} event The event object
+ * @return {string} The formatted starting and ending time
+ */
+function getFormattedTime(event) {
+    var formattedDate = event.startDayjs.format('h:mma');
+    if (event.type === 'event') return formattedDate + event.endDayjs.format(' - h:mma');
+    return formattedDate;
+}
+
+function getFormattedDate(event) {
+    return event.startDayjs.format('dddd · MMMM D, YYYY');
+}
+
+/**
+ * Adds dayjs objects an event object
+ * @param {EventInfo} event An event object
+ */
+function addDayjsElement(e) {
+    // TODO: Add place to change time zone
+    e.startDayjs = convertToTimeZone(e.start, 'America/Chicago');
+    if (e.type === 'event') e.endDayjs = convertToTimeZone(e.end, 'America/Chicago');
+}
+
+/**
+ * Returns a jsx array of filters
+ * @param {object} filters List of filters
+ * @param {boolean} filters.limited Limited slots
+ * @param {boolean} filters.semester Limited slots
+ * @param {boolean} filters.setTimes Limited slots
+ * @param {boolean} filters.weekly Weekly signups (with time)
+ * @param {string} [signupTime] Time that signup will go up, only needed if weekly is true
+ * @returns {JSX.IntrinsicElements[]} jsx array
+ */
+function formatVolunteeringFilters(filters, signupTime) {
+    var filterObjects = [];
+    if (filters.limited)
+        filterObjects.push(
+            <div key="0" className="filter f-limited">
+                Limited Slots
+            </div>
+        );
+    if (filters.semester)
+        filterObjects.push(
+            <div key="1" className="filter f-semester">
+                Semester Long Commitment
+            </div>
+        );
+    if (filters.setTimes)
+        filterObjects.push(
+            <div key="2" className="filter f-set-times">
+                Set Volunteering Times
+            </div>
+        );
+    if (filters.weekly)
+        filterObjects.push(
+            <div key="3" className="filter f-weekly">
+                Weekly Signups [{signupTime}]
+            </div>
+        );
+    return filterObjects;
+}
+ 
+function getMonthAndYear(tz) {
+    return dayjs().format('MMMM YYYY');
+}
+
+function calendarDays() {
+    const date = dayjs().date(1);
+    const year = date.year();
+    const month = date.month();
+    const day = date.day();
+    const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (!(year%400) || !(year%4 && !year%100))
+        days[1]++;
+    var calendar = [];
+    for (let i = 1; i <= days[month]; i++)
+        calendar.push(i);
+    for (let i = day; i > 0; i--)
+        calendar.unshift(days[(month+11)%12]-i);
+    for (let i = (day+days[month])%7, j = 1; i < 7; i++)
+        calendar.push(j++);
+    return calendar;
+}
+
+export {
+    getId,
+    parseTimeZone,
+    convertToTimeZone,
+    divideByDate,
+    createDateHeader,
+    getFormattedTime,
+    getFormattedDate,
+    addDayjsElement,
+    formatVolunteeringFilters,
+    getMonthAndYear,
+    calendarDays,
+};
+
